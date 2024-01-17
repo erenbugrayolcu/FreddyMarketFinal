2abc69dd464aca2e1a54fd07ab4faa439c4d6bda
erenbugrayolcu <yolcubugra7@gmail.com>
2024-01-17 20:22:34 +0300

    urunleri listeleyecek olan URUN_LISTESI kodu yazıldı, port 8080 olarak degistirildi.

--- StokTakipRestWebService/src/main/java/tr/com/freddymarket/webservice/StokTakipWebService.java
+++ StokTakipRestWebService/src/main/java/tr/com/freddymarket/webservice/StokTakipWebService.java
@@ -1,5 +1,22 @@
 package tr.com.freddymarket.webservice;
 
-public class StokTakipWebService {
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
+@RestController
+@RequestMapping("/urun")
+public class StokTakipWebService {
+	
+	private static final List<String> URUN_LISTESI = new ArrayList<>();
+	
+	static {
+		URUN_LISTESI.add("Bakliyat");
+		URUN_LISTESI.add("Sebze");
+	}
+	
+	@GetMapping("/")
+	public List<String> listele(){
+		return URUN_LISTESI;
+	}
+	
 }
