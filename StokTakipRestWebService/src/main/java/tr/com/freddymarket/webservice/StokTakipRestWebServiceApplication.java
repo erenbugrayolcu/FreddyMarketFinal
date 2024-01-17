a1adc685eed81f7cf9003095da0fbb984eddd7d6
erenbugrayolcu <yolcubugra7@gmail.com>
2024-01-17 20:01:13 +0300

    springboot maven proje olusturuldu, .gitignore dosyasi duzenlendi

---
+++ StokTakipRestWebService/src/main/java/tr/com/freddymarket/webservice/StokTakipRestWebServiceApplication.java
@@ -0,0 +1,13 @@
+package tr.com.freddymarket.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StokTakipRestWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StokTakipRestWebServiceApplication.class, args);
+	}
+
+}
---
+++ StokTakipRestWebService/src/test/java/tr/com/freddymarket/webservice/StokTakipRestWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.freddymarket.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StokTakipRestWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
